4f468f3ff0b6250f2956cb659c7a39ec636b405f
ulyssesq <ulyssesq@gmail.com>
2021-02-03 16:40:16 -0300

    Primeiras aulas

---
+++ js-primeiros-passos-com-a-linguagem/1-meuPrimeiroPrograma.js
@@ -0,0 +1,4 @@
+console.log("Meu Primeiro Programa: trabalhando com variáveis");
+
+const idade = 36;
+console.log(idade);
---
+++ js-primeiros-passos-com-a-linguagem/5-atribuicaoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 36;
+const nome = "Ulysses";
+const sobrenome = "Queiroz";
+
+console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
