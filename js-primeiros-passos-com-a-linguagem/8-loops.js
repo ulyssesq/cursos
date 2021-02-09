5fa12c8158e5d778f911fc7313b23d205569e314
Ulysses Queiroz <ulyssesq@ganhejunto.com.br>
2021-02-08 21:30:32 -0300

    Iniciando novo curso

--- js-primeiros-passos-com-a-linguagem/8-loops.js
+++ js-primeiros-passos-com-a-linguagem/8-loops.js
@@ -25,25 +25,24 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada;
 
 let contador = 0;
+let destinoExiste = false;
 
 while (contador < 3) {
     if (listaDeDestinos[contador] == destino) {
         console.log("Destino existe");
-    }
-    else {
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break;
     }
 
     contador++;
 }
 
-console.log("Embarque: \n\n");
+console.log("Destino existe: " + destinoExiste);
 
-if(idadeComprador >= 18 && temPassagemComprada) {
-    console.log("Boa viagem");
+if (podeComprar && destinoExiste) {
+    console.log("Boa Viagem");
 }
 else {
-    console.log("Você não pode embarcar");
+    console.log("Desculpe tivemos um erro!");
 }
 
-console.log(listaDeDestinos);
