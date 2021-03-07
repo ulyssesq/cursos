41a83c8f8ebad8cfd30b5308723785add2ad5ae4
Ulysses Queiroz <ulyssesq@ganhejunto.com.br>
2021-03-07 16:59:42 -0300

    Aula nova'

--- js-interfaces-heranca-oo/index.js
+++ js-interfaces-heranca-oo/index.js
@@ -1,16 +1,12 @@
 import {Cliente} from "./Cliente.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Gerente} from "./Funcionario/Gerente.js";
+import {Diretor} from "./Funcionario/Diretor.js";
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor = new Diretor("Ulysses", 10000, 12345678900);
+diretor.cadastrarSenha("123456789");
+const gerente = new Gerente("Juana",    5000, 12345678901);
+gerente.cadastrarSenha("123")
 
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
-
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
-
-
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo)
+const estaLogado = SistemaAutenticacao.login(gerente, "123");
+console.log(estaLogado);
