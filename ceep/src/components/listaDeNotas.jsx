5675bf0ba12ad8609a960e193fad2ccd41b0692e
Ulysses Queiroz <ulyssesq@ganhejunto.com.br>
2021-03-10 23:34:35 -0300

    teste

--- ceep/src/components/listaDeNotas.jsx
+++ ceep/src/components/listaDeNotas.jsx
@@ -3,8 +3,18 @@ import CardNota from "./cardNota";
 
 class ListaDeNotas extends Component {
     render() {
+        // const numbers = [1,2,3,4,5];
+        // const listItems = numbers.map((number) => <li>{number}</li>);
+        // console.log(listItems);
+
+        // return (
+        //     <ul>
+        //         {listItems}
+        //     </ul>
+        // )
         return (
             <ul>
+
                 {Array.of("Trabalho", "Trabalho", "Estudo").map((categoria) => {
                     return (
                         <li>
