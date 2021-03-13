eb93adf613a7d6b619d4934ab5b705bc3468d85e
Ulysses Queiroz <ulyssesq@ganhejunto.com.br>
2021-03-13 11:38:30 -0300

    Aula 4

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,11 +1,12 @@
-import FormularioCadastro from "./components/formularioCadastro";
-import ListaDeNotas from "./components/listaDeNotas";
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
-  render(){
+  render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
