89077332818a144a7195cd06aef250e56f8840d8
Ulysses Queiroz <ulyssesq@ganhejunto.com.br>
2021-03-14 22:41:40 -0300

    Fim do curso de react

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -3,12 +3,30 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      notas: []
+    };
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas
+    };
+
+    this.setState(novoEstado);
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
