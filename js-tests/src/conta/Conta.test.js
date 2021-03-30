84c6cc14c620d201a0259e16c4cd208b285263c1
Ulysses Queiroz <ulyssesq@gmail.com>
2021-03-30 16:33:41 -0300

    Projeto de teste

---
+++ js-tests/src/conta/Conta.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react'
+import Conta from './conta';
+
+describe('Componente de conta', () => {
+    it('Exibir o saldo da conta com formatação monetária', () => {
+
+        render(<Conta saldo={1000}/>);
+
+        const saldo = screen.getByTestId('saldo-conta');
+        
+
+        expect(saldo.textContent).toBe('R$ 1000');
+    });
+
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+        const funcaoRealizarTransacao = jest.fn();
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao}/>);
+
+        fireEvent.click(screen.getByText('Realizar operação'));
+
+        expect(funcaoRealizarTransacao).toHaveBeenCalled();
+    });
+});
--- styledcomponents/src/Components/Container/index.jsx
+++ styledcomponents/src/Components/Container/index.jsx
@@ -1,17 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import Extrato from "../../Components/Extrato"
 
-const Container = () => {
+const Container = styled.div`
+  background-color: ${({theme}) => theme.body};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  color: ${({theme}) => theme.text};
+
+  @media (max-width: 800px) {
+    .conteudo 
+      flex-direction: column;
+  }
+`;
+
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+        <Extrato />
+      </Conteudo>
+    </Container>
   );
 };
-
-export default Container;
